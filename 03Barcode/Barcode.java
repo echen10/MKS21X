8411041f64e0bb48a80a8c65f13af3a770011257
Evan Chen <echen10@stuy.edu>
2016-12-07 22:16:33 -0500

    Finished the toString for Barcode and tested for exceptions

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -4,21 +4,62 @@ public class Barcode implements Comparable<Barcode> {
 
     public Barcode(String zip) {
 	if ( zip.length() != 5 ) {
-	    throw new IllegalArgumentException("Incorrect zip code length");
+	    throw new IllegalArgumentException("Incorrect zip code length!");
 	}
+	for ( int i = 0; i < 5; i++ ) {
+	    if ( !( Character.isDigit(zip.charAt(i)) ) ) {
+		throw new IllegalArgumentException("Numerical values only!");
+	    }
+	}
+	
 	_zip = zip;
 	_checkDigit = checkSum(_zip);
     }
 
     private int checkSum(String num) {
 	int sum = 0;
-	for ( int i = 0; i < 6; i++ ) {
+	for ( int i = 0; i < 5; i++ ) {
 	    sum += (int) num.charAt(i);
 	}
 	return sum % 10;
     }
 
     public int compareTo(Barcode other) {
-	
+	return this.compareTo(other);
+    }
 
+    public String toString() {
+	String ans = "";
+	ans += _zip + _checkDigit + " |";
+	
+	for ( int i = 0; i < 6; i++ ) {
+	    String bc = _zip + _checkDigit;
+	    int digit = bc.charAt(i);
+
+	    switch( digit ) {
+		
+	    case '1' : ans += ":::||";
+		break;
+	    case '2' : ans += "::|:|";
+		break;
+	    case '3' : ans += "::||:";
+		break;
+	    case '4' : ans += ":|::|";
+		break;
+	    case '5' : ans += ":|:|:";
+		break;
+	    case '6' : ans += ":||::";
+		break;
+	    case '7' : ans += "|:::|";
+		break;
+	    case '8' : ans += "|::|:";
+		break;
+	    case '9' : ans += "|:|::";
+		break;
+	    case '0' : ans += "||:::";
+		break;
+	    }
+	}
+	return ans + "|";
+    }
 }
--- 03Barcode/Driver.java
+++ 03Barcode/Driver.java
@@ -1,5 +1,14 @@
 public class Driver {
     public static void main( String args[] ) {
-	Barcode b1 = new Barcode(123456);
+	Barcode b1 = new Barcode("12345");
+	Barcode b2 = new Barcode("08451");
+	
+	//Barcode b3 = new Barcode("abcde");
+	//Barcode b4 = new Barcode("1234z");
+	//Barcode b5 = new Barcode("w12345");
+	//Barcode b6 = new Barcode("123456");
+	
+	System.out.println(b1);
+	System.out.println(b2);
     }
 }
