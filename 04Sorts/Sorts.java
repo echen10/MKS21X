bb60e98c9bc92ddc503345a4febaf353ac35f131
Evan <echen10@stuy.edu>
2016-12-13 06:25:49 -0500

    Added Sorts.java

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,14 @@
+public class Sorts {
+    public static String name(){
+	return "10.Chen.Evan";
+    }
+    
+    /**Selection sort of an int array. 
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    
+    public static void selectionSort(int[] data) {
+	
+}
+
