3ef3a5f9be8e039d923069e27a76d2cb6544715b
Evan <echen10@stuy.edu>
2016-12-14 06:41:02 -0500

    Worked on insertionSort

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -28,8 +28,10 @@ public class Sorts {
     public static void insertionSort(int[] data) {
 	for ( int i = 1; i < data.length; i++ ) {
 	    int temp = data[i];
-	    for ( int j = data.length; ; j-- ) {
-		data[j+1] = data[j];
+	    for ( int j = i+1; j < data.length; j-- ) {
+		if ( data[i] < data[j] ) {
+		    data[j+1] = data[j];
+		}
 	    }
 	    data[] = temp;
 	}
